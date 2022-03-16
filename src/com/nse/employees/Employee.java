b00770075e61f040059065543750622e60610bc8
ompat <ompat@DESKTOP-LTUJL8L>
2022-03-16 19:44:41 +0530

    static company name created

--- src/com/nse/employees/Employee.java
+++ src/com/nse/employees/Employee.java
@@ -5,12 +5,12 @@ public class Employee {
 	public int employeeNo;
 	public String employeeName;
 	public int employeeSalary;
-	public String companyName;
+	public static String companyName;
 
 	public static void printEmployeeDetails(Employee emp) {
 		System.out.println(emp.employeeNo);
 		System.out.println(emp.employeeName);
 		System.out.println(emp.employeeSalary);
-		System.out.println(emp.companyName);
+		System.out.println(Employee.companyName);
 	}
 }
--- src/com/nse/employees/EmployeeTest.java
+++ src/com/nse/employees/EmployeeTest.java
@@ -4,6 +4,8 @@ public class EmployeeTest {
 
 	public static void main(String[] args) {
 		
+		Employee.companyName = "NSE";
+		
 		Employee emp1 = new Employee();
 		Employee emp2 = new Employee();
 		Employee emp3 = new Employee();
@@ -11,17 +13,17 @@ public class EmployeeTest {
 		emp1.employeeNo = 1001;
 		emp1.employeeName = "Jack";
 		emp1.employeeSalary = 4500;
-		emp1.companyName = "NSE";
+		//emp1.companyName = "NSE";
 		
 		emp2.employeeNo = 1002;
 		emp2.employeeName = "Peter";
 		emp2.employeeSalary = 5000;
-		emp2.companyName = "NSE";
+		//emp2.companyName = "NSE";
 		
 		emp3.employeeNo = 1003;
 		emp3.employeeName = "Mark";
 		emp3.employeeSalary = 9000;
-		emp3.companyName = "NSE";
+		//emp3.companyName = "NSE";
 
 		Employee.printEmployeeDetails(emp2);
 	}
